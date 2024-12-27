06b3135c1f785d7843b615246278ace647d49742
Kihanya <kihanya01@gmail.com>
2024-12-27 12:56:23 +0300

    .

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -22,13 +22,13 @@
   ),
   'livewire/livewire' => 
   array (
-    'providers' => 
-    array (
-      0 => 'Livewire\\LivewireServiceProvider',
-    ),
     'aliases' => 
     array (
       'Livewire' => 'Livewire\\Livewire',
+    ),
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
     ),
   ),
   'livewire/volt' => 
